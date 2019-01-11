1cc8e77cb0091095f7a391a17ae21da4368d33f4
Vender <vender@wewin18.net>
2019-01-11 18:36:11 +0800

    py

--- py/douyu/shuoming.py
+++ py/douyu/shuoming.py
@@ -3,6 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
+from PIL import Image
+from selenium import webdriver
+
 url='https://www.douyu.com/cms/gong/201712/19/6921.shtml'
 headers={
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36'
@@ -19,5 +22,18 @@ def getImg():
         print(img['src'])
 
 
+def getImgs():
+    driver = webdriver.PhantomJS()  #Run in Ubuntu, Windows need set executable_path.
+    driver.maximize_window()
+    driver.get('http://user.qzone.qq.com/{0}/photo'.format(self.qq))
+    driver.implicitly_wait(20)
+    imgs= driver.find_element_by_tag_name('img')
+    for img in imgs:
+        print(img)
+    driver.close()
+    driver.quit()
+
+
+
 if __name__ == "__main__":
-    getImg()
+    getImgs()
